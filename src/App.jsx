728e762a086cfef9475f0a7eb1202152cf23115b
Sriram-S-29 <www.sriramram173@gmail.com>
2024-07-02 10:31:51 +0530

    Calculator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
